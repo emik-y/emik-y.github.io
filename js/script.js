957a010fb9c7e689a1b0ad78ef2fa11416d89d7d
emik-y <emil.yambikov@gmail.com>
2018-03-01 02:16:43 +0500

    index

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function(e) {
+	$('.slider1 .owl-carousel').owlCarousel({
+		items: 3,
+		loop: true,
+		autoplayHoverPause: true,
+		autoplay:true,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+			},
+			1200:{
+				items:2,
+			},
+			1400:{
+				items:3,
+			}
+		}
+	});$('.slider2 .owl-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		// autoplayHoverPause: true,
+		// autoplay:true
+	});
+});
